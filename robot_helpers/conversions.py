e659acd928e4b12bf4545af9bc242712d0ef4a77
Ryotayagi <7522112@ed.tus.ac.jp>
2025-11-03 16:37:32 +0900

    60resolution

--- robot_helpers/conversions.py
+++ robot_helpers/conversions.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def map_cloud_to_grid(voxel_size, points, values):
-    grid = np.zeros((40, 40, 40), dtype=np.float32)
+    grid = np.zeros((60, 60, 60), dtype=np.float32)
     indices = np.round(points / voxel_size).astype(int)
     grid[tuple(indices.T)] = values.squeeze()
     return grid
 
 
-def grid_to_map_cloud(voxel_size, grid, threshold=0.0):
+def grid_to_map_cloud(voxel_size, grid, threshold=0.0):##閾値処理
     grid_shape = grid.shape
     grid_indices = np.indices(grid_shape).reshape(3, -1).T 
     grid_values = grid.flatten() 
