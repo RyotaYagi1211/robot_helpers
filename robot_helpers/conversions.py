a5b4ea8ad18ca768ceea64bcb54d9556d9cc10b0
Ryotayagi <7522112@ed.tus.ac.jp>
2026-02-19 15:26:52 +0900

    notepc

--- robot_helpers/conversions.py
+++ robot_helpers/conversions.py
@@ -2,62 +2,80 @@ import numpy as np
 import open3d as o3d
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  
-def map_cloud_to_grid(voxel_size, points, values):
-    grid = np.zeros((40, 40, 40), dtype=np.float32)
-    # grid = np.zeros((60, 60, 60), dtype=np.float32)##学習時に変更
+# def map_cloud_to_grid(voxel_size, points, values):
+#     grid = np.zeros((40, 40, 40), dtype=np.float32)
+#     # grid = np.zeros((60, 60, 60), dtype=np.float32)##学習時に変更
 
-    indices = np.round(points / voxel_size).astype(int)
-    grid[tuple(indices.T)] = values.squeeze()
-    return grid
+#     indices = np.round(points / voxel_size).astype(int)
+#     grid[tuple(indices.T)] = values.squeeze()
+#     return grid
 
 
-def grid_to_map_cloud(voxel_size, grid, threshold=0.0):
+# def grid_to_map_cloud(voxel_size, grid, threshold=0.0):
+#     grid_shape = grid.shape
+#     grid_indices = np.indices(grid_shape).reshape(3, -1).T 
+#     grid_values = grid.flatten() 
+
+#     mask = grid_values >= threshold
+#     indices = grid_indices[mask]
+#     values = grid_values[mask]
+
+#     points = indices * voxel_size
+#     values = values[:, None] 
+#     return points, values
+
+
+def grid_to_map_cloud(voxel_size, grid, threshold=0.0, origin=None):
+
     grid_shape = grid.shape
-    grid_indices = np.indices(grid_shape).reshape(3, -1).T 
-    grid_values = grid.flatten() 
+    grid_indices = np.indices(grid_shape).reshape(3, -1).T
+    grid_values = grid.flatten()
 
     mask = grid_values >= threshold
     indices = grid_indices[mask]
-    values = grid_values[mask]
+    values = grid_values[mask][:, None]
 
-    points = indices * voxel_size
-    values = values[:, None] 
+    points = (indices.astype(np.float32) + 0.5) * float(voxel_size) + origin
     return points, values
 
+def map_cloud_to_grid(voxel_size, points, values, grid_shape=(40,40,40), origin=None):
+
+    grid = np.zeros(grid_shape, dtype=np.float32)
+
+    indices = np.floor((points - origin) / float(voxel_size)).astype(int)
+
+    valid = np.all((indices >= 0) & (indices < np.array(grid_shape)), axis=1)
+    indices = indices[valid]
+    values = values[valid].reshape(-1)
+
+    grid[indices[:,0], indices[:,1], indices[:,2]] = values
+    return grid
 
 
 
-
-def map_clouds_to_grid_instance(voxel_size, points, instance_ids, values=None, grid_shape=(60, 60, 60)):
+def map_clouds_to_grid_instance(voxel_size, points, instance_ids, values=None,
+                                grid_shape=(40, 40, 40), origin=None):
     unique_ids = np.unique(instance_ids)
     grid = np.zeros((grid_shape[0], grid_shape[1], grid_shape[2], len(unique_ids)), dtype=np.float32)
-    indices = np.round(points / voxel_size).astype(int)
+
+    ###
+    indices = np.floor((points - origin) / float(voxel_size)).astype(int)
+
     valid_mask = np.all((indices >= 0) & (indices < np.array(grid_shape)), axis=1)
     indices = indices[valid_mask]
     instance_ids = instance_ids[valid_mask]
     if values is not None:
-        values = values[valid_mask]
+        values = values[valid_mask].reshape(-1)
 
     for i, uid in enumerate(unique_ids):
         mask = (instance_ids == uid)
         idx = indices[mask]
-        grid[..., i][tuple(idx.T)] = values[mask].squeeze()
-
-    pts_list, colors_list = [], []
-    rng = np.random.RandomState(0)
-    cmap = rng.rand(len(unique_ids), 3)
-
-    for i in range(len(unique_ids)):
-        grid_i = grid[i]
-        mask = grid_i > 0.0
-        if not np.any(mask):
+        if idx.size == 0:
             continue
-        idx = np.argwhere(mask)
-        pts = idx.astype(np.float32) 
-        col = np.tile(cmap[i], (pts.shape[0], 1))
-        pts_list.append(pts)
-        colors_list.append(col)
-
+        if values is None:
+            grid[idx[:, 0], idx[:, 1], idx[:, 2], i] = 1.0
+        else:
+            grid[idx[:, 0], idx[:, 1], idx[:, 2], i] = values[mask]
 
 ##visualize all instances together
     # all_pts = np.vstack(pts_list)
@@ -77,7 +95,7 @@ def map_clouds_to_grid_instance(voxel_size, points, instance_ids, values=None, g
     return grid, unique_ids
 
 
-def grid_to_map_clouds_instance(voxel_size, occupancy_voxel_instance, threshold=0.0):
+def grid_to_map_clouds_instance(voxel_size, occupancy_voxel_instance, threshold=0.0,origin=None):
     X, Y, Z, num_instances = occupancy_voxel_instance.shape
 
     points_list, values_list, ids_list = [], [], []
@@ -89,7 +107,8 @@ def grid_to_map_clouds_instance(voxel_size, occupancy_voxel_instance, threshold=
             continue
 
         idx = np.argwhere(mask)  # (K, 3)
-        pts = idx.astype(np.float32) * float(voxel_size)
+        # pts = idx.astype(np.float32) * float(voxel_size)
+        pts = (idx.astype(np.float32) + 0.5) * float(voxel_size) + origin
         vals = grid_i[mask].astype(np.float32)
 
         points_list.append(pts)
